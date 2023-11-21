731225371aaace9852616b10d11271882b8f5f8f
Emrecan Ay <codemrecan@gmail.com>
2023-11-22 01:30:09 +0300

    Entity Framework and PostgreSQL relation created! Car service Add, GetAll tested successfully!

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+
+Car car = new Car{ BrandId=1, ColorId=4, CarName="Test Car 2", DailyPrice=250, ModelYear=2021, Description="Test description 2."  };
+
+CarManager carManager = new CarManager(new EfCarDal());
+
+AddTest(car, carManager);
+
+static void AddTest(Car car, CarManager carManager)
+{
+    try
+    {
+        carManager.Add(car);
+        Console.WriteLine("Success!");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+static void GetAllTest(CarManager carManager)
+{
+    carManager.GetAll().ForEach(car =>
+    {
+        Console.WriteLine(car.CarName);
+    });
+}
---
+++ DataAccess/Concrete/EntityFramework/Contexts/RentACarContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class RentACarContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=RentACar;Trusted_Connection=true");
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=123;Host=localhost;Port=5433;Database=RentACar;");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
